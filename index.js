ecd25d4974a1b1d3191dc783b3014f46b38ebf39
SluvsP <salomekbg@gmail.com>
2016-11-15 15:25:10 -0500

    Done.

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ const list =
       ]
     ));
 
-ReactDOM.render(list, document.getElementById('main'));
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
+
+ReactDOM.render(container, document.getElementById('main'));
